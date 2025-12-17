448062871579aa6e580fa9a7b59bdfacb39c22e3
Nurahfezan Nordin <nurahfezanbinnordin@gmail.com>
2025-12-17 10:58:03 +0800

    feat: refactor event voucher page

---
+++ src/components/pages/vouchers/table/event-voucher-table-control.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import type { Table } from "@tanstack/react-table";
+import { BaseTableControl } from "@/components/admin-ui/table/control/base-table-control";
+import type { ControlConfig } from "@/components/admin-ui/table/control/type";
+
+interface DataControlProps<TData> {
+	table: Table<TData>;
+}
+
+export function DataControl<TData>({ table }: DataControlProps<TData>) {
+	const getStatusFilterValue = () => {
+		const statusFilter = table.getColumn("status")?.getFilterValue() as
+			| string
+			| undefined;
+		return statusFilter ?? "all";
+	};
+
+	const statusFilterControl: ControlConfig = {
+		label: "Status",
+		columnId: "status",
+		type: "filter",
+		data: [
+			{ label: "All", value: "all" },
+			{ label: "Active", value: "active" },
+			{ label: "Inactive", value: "inactive" },
+			{ label: "Expired", value: "expired" },
+		],
+		customFilter: {
+			value: getStatusFilterValue(),
+			onChange: (value: string) => {
+				const column = table.getColumn("status");
+				column?.setFilterValue(value === "all" ? undefined : value);
+			},
+		},
+	};
+
+	const desktopControlConfigs: ControlConfig[] = [
+		statusFilterControl,
+		{
+			label: "Columns",
+			columnId: "visibility",
+			type: "visibility",
+		},
+	];
+
+	const mobileControlConfigs: ControlConfig[] = [
+		{ ...statusFilterControl, topPriority: true },
+		{ label: "Title", columnId: "title", type: "sort" },
+		{ label: "Validity", columnId: "validity", type: "sort" },
+	];
+
+	return (
+		<BaseTableControl
+			table={table}
+			searchConfig={{
+				searchConfig: {
+					placeholder: "Search vouchers...",
+					enableCustomSearch: true,
+					columns: ["title", "merchant"],
+				},
+			}}
+			desktopConfig={{
+				controlConfigs: desktopControlConfigs,
+			}}
+			mobileConfig={{
+				controlConfigs: mobileControlConfigs,
+			}}
+		/>
+	);
+}
