402c0b8ac7edcc90b2a9cb370face3aef7c1c82a
Nurahfezan Nordin <nurahfezanbinnordin@gmail.com>
2025-12-30 10:27:34 +0800

    feat: create sitemap generation

---
+++ src/app/sitemap.ts
@@ -0,0 +1,40 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const baseUrl = "https://eventzflow.com";
+
+	const staticPages = [
+		{
+			url: baseUrl,
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 1,
+		},
+		{
+			url: `${baseUrl}/auth`,
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.8,
+		},
+		{
+			url: `${baseUrl}/about`,
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.8,
+		},
+		{
+			url: `${baseUrl}/privacy-policy`,
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.3,
+		},
+		{
+			url: `${baseUrl}/terms-of-service`,
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.3,
+		},
+	];
+
+	return Promise.resolve([...staticPages] as MetadataRoute.Sitemap);
+}
